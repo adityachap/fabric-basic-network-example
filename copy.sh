7857f37d2ec02351e2306a2fd1d755f90b66f197
muledeepak <deepak.mule@nttdata.com>
2017-08-31 14:43:07 +0530

    update

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-set -ev
+
 mkdir -p /etc/hyperledger/fabric-ca-server-config/
 cp -r /var/codebase/fabric-basic-network-example/crypto-config/peerOrganizations/org1.example.com/ca/  /etc/hyperledger/fabric-ca-server-config/
 mkdir -p /etc/hyperledger/test/
